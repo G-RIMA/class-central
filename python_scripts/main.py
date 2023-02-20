d56cff57537526dffabfc438d64198050b43b006
g-rima <gikandimaria178@gmail.com>
2023-02-20 13:54:06 +0300

    webscraped classcentral

---
+++ python_scripts/main.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+url = "https://www.classcentral.com/provider/unccelearn"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Raise an exception for non-200 response codes
+except requests.exceptions.RequestException as e:
+    print("Error: Failed to fetch the homepage HTML")
+    print(e)
+else:
+    html_content = response.text
+    # do something with the HTML content
+    with open("unccelearn.html", "w", encoding="utf-8") as file:
+        file.write(html_content)
+
+
+
